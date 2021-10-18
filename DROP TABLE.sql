5b3da39d17baf76adc5ae37004924978d2e3a3bc
Чесноков Сергей <spyquail5@gmail.com>
2021-10-18 22:38:31 +0700

    1

--- DROP TABLE.sql
+++ DROP TABLE.sql
@@ -1,9 +1,9 @@
 drop table CITIES cascade constraints;
 drop table HOSPITALS cascade constraints;
-drop table MED_ORGANIZATION cascade constraints;
+drop table MED_ORGANIZATIONs cascade constraints;
 drop table PATIENTS cascade constraints;
 drop table QUALIFICATIONS cascade constraints;
-drop table REGION cascade constraints;
+drop table REGIONs cascade constraints;
 drop table SPECIALTIES cascade constraints;
 drop table TYPES_HOSPITAL cascade constraints;
 drop table USERS cascade constraints;
@@ -12,6 +12,6 @@ drop table TALONS cascade constraints;
 drop table JOURNAL cascade constraints;
 drop table DOCTORS cascade constraints;
 drop table DOCTOR_SPECIALTIES cascade constraints;
-drop table GENDER cascade constraints;
-drop table PROFILE_WORKING_HOURS cascade constraints;
+drop table GENDERs cascade constraints;
+drop table WORKING_TIME cascade constraints;
 
--- скрипт вставки данных, на которых проводились выборки.sql
+++ скрипт вставки данных, на которых проводились выборки.sql
@@ -1,63 +1,90 @@
-INSERT INTO region(region_name) VALUES ('Кемеровская область');
-INSERT INTO region(region_name) VALUES ('Московская область');
-INSERT INTO region(region_name) VALUES ('Новоибирская область');
-INSERT INTO region(region_name) VALUES ('Красноярская область');
-INSERT INTO region(region_name) VALUES ('Томская область');
-INSERT INTO region(region_name) VALUES ('Краснодарский край');
-INSERT INTO region(region_name) VALUES ('Ленинградская область');
-INSERT INTO region(region_name) VALUES ('Пермский край');
-INSERT INTO region(region_name) VALUES ('Приморский край');
-INSERT INTO region(region_name) VALUES ('Тверская область');
+INSERT INTO regions(region_name) VALUES ('Кемеровская область');
+INSERT INTO regions(region_name) VALUES ('Московская область');
+INSERT INTO regions(region_name) VALUES ('Новоибирская область');
+INSERT INTO regions(region_name) VALUES ('Красноярская область');
+INSERT INTO regions(region_name) VALUES ('Томская область');
+INSERT INTO regions(region_name) VALUES ('Краснодарский край');
+INSERT INTO regions(region_name) VALUES ('Ленинградская область');
+INSERT INTO regions(region_name) VALUES ('Пермский край');
+INSERT INTO regions(region_name) VALUES ('Приморский край');
+INSERT INTO regions(region_name) VALUES ('Тверская область');
 
 
-INSERT INTO cities(city_name, region_id) VALUES ('Юрга', 1);
-INSERT INTO cities(city_name, region_id) VALUES ('Москва', 2);
-INSERT INTO cities(city_name, region_id) VALUES ('Новосибирск', 3);
-INSERT INTO cities(city_name, region_id) VALUES ('Красноярск', 4);
-INSERT INTO cities(city_name, region_id) VALUES ('Томск', 5);
-INSERT INTO cities(city_name, region_id) VALUES ('Краснодар', 6);
-INSERT INTO cities(city_name, region_id) VALUES ('Санкт-Петербург', 7);
-INSERT INTO cities(city_name, region_id) VALUES ('Пермь', 8);
-INSERT INTO cities(city_name, region_id) VALUES ('Владивосток', 9);
-INSERT INTO cities(city_name, region_id) VALUES ('Тверь', 10);
+INSERT INTO cities(city_name, id_region) VALUES ('Юрга', 1);
+INSERT INTO cities(city_name, id_region) VALUES ('Москва', 2);
+INSERT INTO cities(city_name, id_region) VALUES ('Новосибирск', 3);
+INSERT INTO cities(city_name, id_region) VALUES ('Красноярск', 4);
+INSERT INTO cities(city_name, id_region) VALUES ('Томск', 5);
+INSERT INTO cities(city_name, id_region) VALUES ('Краснодар', 6);
+INSERT INTO cities(city_name, id_region) VALUES ('Санкт-Петербург', 7);
+INSERT INTO cities(city_name, id_region) VALUES ('Пермь', 8);
+INSERT INTO cities(city_name, id_region) VALUES ('Владивосток', 9);
+INSERT INTO cities(city_name, id_region) VALUES ('Тверь', 10);
 
 
-INSERT INTO med_organization(organization_name, city_id) VALUES ('Премиум', 2);
-INSERT INTO med_organization(organization_name, city_id) VALUES ('Старт', 3);
-INSERT INTO med_organization(organization_name, city_id) VALUES ('Здоровье', 4);
-INSERT INTO med_organization(organization_name, city_id) VALUES ('Сердце', 5);
-INSERT INTO med_organization(organization_name, city_id) VALUES ('Доктор', 6);
-INSERT INTO med_organization(organization_name, city_id) VALUES ('Новелла', 7);
-INSERT INTO med_organization(organization_name, city_id) VALUES ('Health', 8);
-INSERT INTO med_organization(organization_name, city_id) VALUES ('Лечение', 9);
-INSERT INTO med_organization(organization_name, city_id) VALUES ('Альфа', 10);
-INSERT INTO med_organization(organization_name, city_id) VALUES ('Бета1', 10);
+INSERT INTO med_organizations(organization_name, id_city) VALUES ('Премиум', 2);
+INSERT INTO med_organizations(organization_name, id_city) VALUES ('Старт', 3);
+INSERT INTO med_organizations(organization_name, id_city) VALUES ('Здоровье', 4);
+INSERT INTO med_organizations(organization_name, id_city) VALUES ('Сердце', 5);
+INSERT INTO med_organizations(organization_name, id_city) VALUES ('Доктор', 6);
+INSERT INTO med_organizations(organization_name, id_city) VALUES ('Новелла', 7);
+INSERT INTO med_organizations(organization_name, id_city) VALUES ('Health', 8);
+INSERT INTO med_organizations(organization_name, id_city) VALUES ('Лечение', 9);
+INSERT INTO med_organizations(organization_name, id_city) VALUES ('Альфа', 10);
+INSERT INTO med_organizations(organization_name, id_city) VALUES ('Бета1', 10);
 
 
 INSERT INTO types_hospital(type_name) VALUES ('Частная');
 INSERT INTO types_hospital(type_name) VALUES ('Государственная');
 
 
-insert into PROFILE_WORKING_HOURS(monday_start_time, monday_end_time, tuesday_start_time, tuesday_end_time, wednesday_start_time, wednesday_end_time, thursday_start_time, thursday_end_time, friday_start_time, friday_end_time, saturday_start_time, saturday_end_time, sunday_start_time, sunday_end_time) values (28800, 64800, 28800, 64800, 28800, 64800, 28800, 64800, 28800, 64800, 28800, 64800, 28800, 64800);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (1,to_date('08:00:00','hh24:mi:ss'), to_date('18:00:00','hh24:mi:ss'), 1);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (2,to_date('08:00:00','hh24:mi:ss'), to_date('18:00:00','hh24:mi:ss'), 1);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (3,to_date('08:00:00','hh24:mi:ss'), to_date('18:00:00','hh24:mi:ss'), 1);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (4,to_date('08:00:00','hh24:mi:ss'), to_date('18:00:00','hh24:mi:ss'), 1);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (5,to_date('08:00:00','hh24:mi:ss'), to_date('18:00:00','hh24:mi:ss'), 1);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (6,to_date('08:00:00','hh24:mi:ss'), to_date('16:00:00','hh24:mi:ss'), 1);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (1,to_date('08:00:00','hh24:mi:ss'), to_date('18:00:00','hh24:mi:ss'), 2);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (2,to_date('08:00:00','hh24:mi:ss'), to_date('18:00:00','hh24:mi:ss'), 2);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (3,to_date('08:00:00','hh24:mi:ss'), to_date('18:00:00','hh24:mi:ss'), 2);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (4,to_date('08:00:00','hh24:mi:ss'), to_date('18:00:00','hh24:mi:ss'), 2);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (5,to_date('08:00:00','hh24:mi:ss'), to_date('18:00:00','hh24:mi:ss'), 2);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (6,to_date('08:00:00','hh24:mi:ss'), to_date('16:00:00','hh24:mi:ss'), 2);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (1,to_date('08:00:00','hh24:mi:ss'), to_date('18:00:00','hh24:mi:ss'), 3);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (2,to_date('08:00:00','hh24:mi:ss'), to_date('18:00:00','hh24:mi:ss'), 3);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (3,to_date('08:00:00','hh24:mi:ss'), to_date('18:00:00','hh24:mi:ss'), 3);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (4,to_date('08:00:00','hh24:mi:ss'), to_date('18:00:00','hh24:mi:ss'), 3);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (5,to_date('08:00:00','hh24:mi:ss'), to_date('18:00:00','hh24:mi:ss'), 3);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (6,to_date('08:00:00','hh24:mi:ss'), to_date('16:00:00','hh24:mi:ss'), 3);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (1,to_date('08:00:00','hh24:mi:ss'), to_date('18:00:00','hh24:mi:ss'), 4);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (2,to_date('08:00:00','hh24:mi:ss'), to_date('18:00:00','hh24:mi:ss'), 4);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (3,to_date('08:00:00','hh24:mi:ss'), to_date('18:00:00','hh24:mi:ss'), 4);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (4,to_date('08:00:00','hh24:mi:ss'), to_date('18:00:00','hh24:mi:ss'), 4);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (5,to_date('08:00:00','hh24:mi:ss'), to_date('18:00:00','hh24:mi:ss'), 4);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (1,to_date('08:00:00','hh24:mi:ss'), to_date('18:00:00','hh24:mi:ss'), 4);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (2,to_date('08:00:00','hh24:mi:ss'), to_date('18:00:00','hh24:mi:ss'), 5);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (3,to_date('08:00:00','hh24:mi:ss'), to_date('18:00:00','hh24:mi:ss'), 5);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (4,to_date('08:00:00','hh24:mi:ss'), to_date('18:00:00','hh24:mi:ss'), 5);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (5,to_date('08:00:00','hh24:mi:ss'), to_date('18:00:00','hh24:mi:ss'), 5);
+insert into WORKING_TIME(day, begin_time, end_time, id_hospital) values (6,to_date('08:00:00','hh24:mi:ss'), to_date('16:00:00','hh24:mi:ss'), 5);
+
+INSERT INTO hospitals(hospital_name, id_organization, id_type, AVAILABILITY) VALUES ('Больница №1', 2, 2, 1);
+INSERT INTO hospitals(hospital_name, id_organization, id_type, delete_dt, AVAILABILITY) VALUES ('Больница №2', 3, 1, '28.09.2021', 1);
+INSERT INTO hospitals(hospital_name, id_organization, id_type, AVAILABILITY) VALUES ('Больница №3', 4, 2, 1);
+INSERT INTO hospitals(hospital_name, id_organization, id_type, AVAILABILITY) VALUES ('Больница №4', 5, 1, 1);
+INSERT INTO hospitals(hospital_name, id_organization, id_type, AVAILABILITY) VALUES ('Больница №5', 6, 2, 1);
 
 
-INSERT INTO hospitals(hospital_name, organization_id, type_id, WORKING_HOURS_ID, AVAILABILITY) VALUES ('Больница №1', 2, 2, 1,'true');
-INSERT INTO hospitals(hospital_name, organization_id, type_id, WORKING_HOURS_ID, delete_dt, AVAILABILITY) VALUES ('Больница №2', 3, 1, 1, '28.09.2021', 'true');
-INSERT INTO hospitals(hospital_name, organization_id, type_id, WORKING_HOURS_ID, AVAILABILITY) VALUES ('Больница №3', 4, 2, 1, 'true');
-INSERT INTO hospitals(hospital_name, organization_id, type_id, WORKING_HOURS_ID, AVAILABILITY) VALUES ('Больница №4', 5, 1, 1, 'true');
-INSERT INTO hospitals(hospital_name, organization_id, type_id, WORKING_HOURS_ID, AVAILABILITY) VALUES ('Больница №5', 6, 2, 1, 'true');
+insert into genders(gender) values ('Мужской');
+insert into genders(gender) values ('Женский');
+insert into genders(gender) values ('Любой');
 
 
-insert into gender(gender) values ('Мужской');
-insert into gender(gender) values ('Женский');
-insert into gender(gender) values ('Любой');
-
-
-INSERT INTO specialties(speciality_name, REQUIRED_GENDER_ID, REQUIRED_MIN_AGE, REQUIRED_MAX_AGE) VALUES ('Хирург', 3, 18, 120);
-INSERT INTO specialties(speciality_name, REQUIRED_GENDER_ID, REQUIRED_MIN_AGE, REQUIRED_MAX_AGE, DELETE_DT) VALUES ('Терапевт', 3, 18, 120, '20.07.2021');
-INSERT INTO specialties(speciality_name, REQUIRED_GENDER_ID, REQUIRED_MIN_AGE, REQUIRED_MAX_AGE) VALUES ('ЛОР', 3, 0, 120);
-INSERT INTO specialties(speciality_name, REQUIRED_GENDER_ID, REQUIRED_MIN_AGE, REQUIRED_MAX_AGE) VALUES ('Педиатр', 3, 0, 18);
-INSERT INTO specialties(speciality_name, REQUIRED_GENDER_ID, REQUIRED_MIN_AGE, REQUIRED_MAX_AGE) VALUES ('Гинеколог', 2, 0, 120);
+INSERT INTO specialties(speciality_name, ID_REQUIRED_GENDER, REQUIRED_MIN_AGE, REQUIRED_MAX_AGE) VALUES ('Хирург', 3, 18, 120);
+INSERT INTO specialties(speciality_name, ID_REQUIRED_GENDER, REQUIRED_MIN_AGE, REQUIRED_MAX_AGE, DELETE_DT) VALUES ('Терапевт', 3, 18, 120, '20.07.2021');
+INSERT INTO specialties(speciality_name, ID_REQUIRED_GENDER, REQUIRED_MIN_AGE, REQUIRED_MAX_AGE) VALUES ('ЛОР', 3, 0, 120);
+INSERT INTO specialties(speciality_name, ID_REQUIRED_GENDER, REQUIRED_MIN_AGE, REQUIRED_MAX_AGE) VALUES ('Педиатр', 3, 0, 18);
+INSERT INTO specialties(speciality_name, ID_REQUIRED_GENDER, REQUIRED_MIN_AGE, REQUIRED_MAX_AGE) VALUES ('Гинеколог', 2, 0, 120);
 
 
 INSERT INTO qualifications(qualification_name, QUALIFICATION_LEVEL) VALUES ('Эксперт', 5);
@@ -74,18 +101,18 @@ INSERT INTO AREAS(area_name) VALUES ('Четвертый');
 INSERT INTO AREAS(area_name) VALUES ('Пятый');
 
 
-INSERT INTO DOCTORS(hospital_id, area_id, qualification_id, doctor_name) VALUES (1, 1, 3, 'Пермяков Иван');
-INSERT INTO DOCTORS(hospital_id, area_id, qualification_id, doctor_name, DELETE_DT) VALUES (2, 2, 4, 'Иванов Иван', '15.06.2020');
-INSERT INTO DOCTORS(hospital_id, area_id, qualification_id, doctor_name) VALUES (3, 3, 5, 'Андреев Иван');
-INSERT INTO DOCTORS(hospital_id, area_id, qualification_id, doctor_name) VALUES (4, 4, 5, 'Ступкин Иван');
-INSERT INTO DOCTORS(hospital_id, area_id, qualification_id, doctor_name) VALUES (5, 5, 1, 'Резцов Дмитрий');
+INSERT INTO DOCTORS(id_hospital, id_area, id_qualification, doctor_name) VALUES (1, 1, 3, 'Пермяков Иван');
+INSERT INTO DOCTORS(id_hospital, id_area, id_qualification, doctor_name, DELETE_DT) VALUES (2, 2, 4, 'Иванов Иван', '15.06.2020');
+INSERT INTO DOCTORS(id_hospital, id_area, id_qualification, doctor_name) VALUES (3, 3, 5, 'Андреев Иван');
+INSERT INTO DOCTORS(id_hospital, id_area, id_qualification, doctor_name) VALUES (4, 4, 5, 'Ступкин Иван');
+INSERT INTO DOCTORS(id_hospital, id_area, id_qualification, doctor_name) VALUES (5, 5, 1, 'Резцов Дмитрий');
 
 
-INSERT INTO TALONS(START_date, end_date, doctor_id) VALUES ('28.09.2021', '17.10.2021', 1);
-INSERT INTO TALONS(DELETE_DT, START_date, end_date, doctor_id) VALUES ('25.09.2021', '26.09.2021', '17.10.2021', 5);
-INSERT INTO TALONS(START_date, end_date, doctor_id) VALUES ('23.09.2021', '27.10.2021', 2);
-INSERT INTO TALONS(START_date, end_date, doctor_id) VALUES ('30.09.2021', '17.10.2021', 3);
-INSERT INTO TALONS(START_date, end_date, doctor_id) VALUES ('7.10.2021', '17.10.2021', 5);
+INSERT INTO TALONS(START_date, end_date, id_doctor, availability ) VALUES ('28.09.2021', '17.10.2021', 1, 1);
+INSERT INTO TALONS(DELETE_DT, START_date, end_date, id_doctor, availability ) VALUES ('25.09.2021', '26.09.2021', '17.10.2021', 5, 1);
+INSERT INTO TALONS(START_date, end_date, id_doctor, availability ) VALUES ('23.09.2021', '27.10.2021', 2, 1);
+INSERT INTO TALONS(START_date, end_date, id_doctor, availability ) VALUES ('30.09.2021', '17.10.2021', 3, 1);
+INSERT INTO TALONS(START_date, end_date, id_doctor, availability ) VALUES ('7.10.2021', '17.10.2021', 5, 1);
 
 
 INSERT INTO USERS(login) VALUES ('user1');
@@ -95,22 +122,22 @@ INSERT INTO USERS(login) VALUES ('lifer');
 INSERT INTO USERS(login) VALUES ('rbq');
 
 
-INSERT INTO patients(surname, name, middle_name, date_birth, serial_doc, gender_id, phone_number, area_id, user_id) values ('Тройский', 'Алексей', 'андреевич', '17.05.1995', 123433, 1, 79991234567, 1, 1);
-INSERT INTO patients(surname, name, middle_name, date_birth, serial_doc, GENDER_ID, phone_number, area_id, user_id) values ('Иванов', 'Алексей', 'андреевич', '07.03.1993', 123663, 1, 79991234567, 2, 2);
-INSERT INTO patients(surname, name, middle_name, date_birth, serial_doc, GENDER_ID, phone_number, area_id, user_id) values ('Петров', 'Алексей', 'андреевич', '13.04.1992', 127723, 1, 79991234567, 3, 3);
-INSERT INTO patients(surname, name, middle_name, date_birth, serial_doc, GENDER_ID, phone_number, area_id, user_id) values ('Баженов', 'Евгений', 'Владимирович', '24.05.1991', 128123, 1, 79991234567, 4, 4);
-INSERT INTO patients(surname, name, middle_name, date_birth, serial_doc, GENDER_ID, phone_number, area_id, user_id) values ('Куплинов', 'Дмитрий', 'Алексеевич', '25.11.1988', 129923, 1, 79991234567, 5, 5);
+INSERT INTO patients(surname, name, middle_name, date_birth, serial_doc, id_gender, phone_number, id_area, id_user) values ('Тройский', 'Алексей', 'андреевич', '17.05.1995', 123433, 1, 79991234567, 1, 1);
+INSERT INTO patients(surname, name, middle_name, date_birth, serial_doc, id_gender, phone_number, id_area, id_user) values ('Иванов', 'Алексей', 'андреевич', '07.03.1993', 123663, 1, 79991234567, 2, 2);
+INSERT INTO patients(surname, name, middle_name, date_birth, serial_doc, id_gender, phone_number, id_area, id_user) values ('Петров', 'Алексей', 'андреевич', '13.04.1992', 127723, 1, 79991234567, 3, 3);
+INSERT INTO patients(surname, name, middle_name, date_birth, serial_doc, id_gender, phone_number, id_area, id_user) values ('Баженов', 'Евгений', 'Владимирович', '24.05.1991', 128123, 1, 79991234567, 4, 4);
+INSERT INTO patients(surname, name, middle_name, date_birth, serial_doc, id_gender, phone_number, id_area, id_user) values ('Куплинов', 'Дмитрий', 'Алексеевич', '25.11.1988', 129923, 1, 79991234567, 5, 5);
 
 
-INSERT INTO journal(patient_id, datetime, talon_id) VALUES (1, '30.09.2021', 1);
-INSERT INTO journal(patient_id, datetime, talon_id) VALUES (2, '30.09.2021', 2);
-INSERT INTO journal(patient_id, datetime, talon_id) VALUES (3, '30.09.2021', 3);
-INSERT INTO journal(patient_id, datetime, talon_id, DELETE_DT) VALUES (4, '30.09.2021', 4, '29.09.2021');
+INSERT INTO journal(id_patient, datetime, id_talon, availability ) VALUES (1, '30.09.2021', 1, 1);
+INSERT INTO journal(id_patient, datetime, id_talon, availability ) VALUES (2, '30.09.2021', 2, 1);
+INSERT INTO journal(id_patient, datetime, id_talon, availability ) VALUES (3, '30.09.2021', 3, 1);
+INSERT INTO journal(id_patient, datetime, id_talon, DELETE_DT, availability ) VALUES (4, '30.09.2021', 4, '29.09.2021', 1);
 
-insert into DOCTOR_SPECIALTIES(doctor_id, specialty_id) values (1, 1);
-insert into DOCTOR_SPECIALTIES(doctor_id, specialty_id) values (1, 2);
-insert into DOCTOR_SPECIALTIES(doctor_id, specialty_id) values (2, 3);
-insert into DOCTOR_SPECIALTIES(doctor_id, specialty_id) values (2, 5);
-insert into DOCTOR_SPECIALTIES(doctor_id, specialty_id) values (3, 1);
-insert into DOCTOR_SPECIALTIES(doctor_id, specialty_id) values (4, 4);
-insert into DOCTOR_SPECIALTIES(doctor_id, specialty_id) values (5, 1);
+insert into DOCTOR_SPECIALTIES(id_doctor, id_specialty) values (1, 1);
+insert into DOCTOR_SPECIALTIES(id_doctor, id_specialty) values (1, 2);
+insert into DOCTOR_SPECIALTIES(id_doctor, id_specialty) values (2, 3);
+insert into DOCTOR_SPECIALTIES(id_doctor, id_specialty) values (2, 5);
+insert into DOCTOR_SPECIALTIES(id_doctor, id_specialty) values (3, 1);
+insert into DOCTOR_SPECIALTIES(id_doctor, id_specialty) values (4, 4);
+insert into DOCTOR_SPECIALTIES(id_doctor, id_specialty) values (5, 1);
