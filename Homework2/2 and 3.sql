d49b0daceffaf6ae094af1d38cf2d5f4b3aaf19f
Чесноков Сергей <spyquail5@gmail.com>
2021-10-19 20:19:06 +0700

    1

--- Homework2/2 and 3.sql
+++ Homework2/2 and 3.sql
@@ -1,15 +1,16 @@
 declare
     v_temp_char char(39);
     v_temp_number number;
+    v_variable number := 1;
 begin
     select chesnokov_se.QUALIFICATIONS.QUALIFICATION_LEVEL
     into v_temp_char
     from chesnokov_se.QUALIFICATIONS
-    where CHESNOKOV_SE.QUALIFICATIONS.ID_QUALIFICATION = 1;
+    where CHESNOKOV_SE.QUALIFICATIONS.ID_QUALIFICATION = v_variable;
     select chesnokov_se.QUALIFICATIONS.QUALIFICATION_LEVEL
     into v_temp_number
     from chesnokov_se.QUALIFICATIONS
-    where CHESNOKOV_SE.QUALIFICATIONS.ID_QUALIFICATION = 1;
+    where CHESNOKOV_SE.QUALIFICATIONS.ID_QUALIFICATION = v_variable;
     dbms_output.put_line(v_temp_char);
     dbms_output.put_line(v_temp_number);
 end;
